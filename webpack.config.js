a89c0bfa4b5ff38dda6f51a8a146615e5306d3fe
Riley Mathews <rileymathews80@gmail.com>
2019-05-23 17:54:30 -0500

    added dev server config

--- webpack.config.js
+++ webpack.config.js
@@ -6,4 +6,19 @@ module.exports = {
       title: "Webpack demo",
     }),
   ],
+  devServer: {
+    // Display only errors to reduce the amount of output.
+    stats: "errors-only",
+    host: "0.0.0.0", // Defaults to `localhost`
+    port: process.env.PORT, // Defaults to 8080
+    open: true, // Open the page in browser
+    overlay: true,
+    watchOptions: {
+      // Delay the rebuild after the first change
+      aggregateTimeout: 300,
+
+      // Poll using interval (in ms, accepts boolean too)
+      poll: 1000,
+    },
+  },
 };
